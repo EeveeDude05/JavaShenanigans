ccc041f6f2a1d1bfda71b38ffdac67f4b70ad68a
alphawolf05 <legodudea@outlook.com>
2026-01-28 22:42:27 -0700

    BATMANNNNN

---
+++ batman.java
@@ -0,0 +1,9 @@
+// This is a comment
+// this code will print this message to the console
+// I don't do feelings alfred, I'm a crime fighting, night stalking vigilante! I dont feel anything exept for rage! 24/7, 365, at a million percent! and if you think theres somthing under that than your wrong!
+public class batman
+ {
+    public static void main(String[] args) {
+        System.out.println("I don't do feelings Alfred, I'm a crime fighting, night stalking vigilante! I don't feel anything except for rage! 24/7, 365, at a million percent! And if you think there's something under that then you're wrong!");
+    }
+}
