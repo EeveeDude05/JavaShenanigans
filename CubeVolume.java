d1807f94b3f93c667cad89996809c1e220cb8434
alphawolf05 <legodudea@outlook.com>
2026-02-05 13:09:41 -0700

    Bug fixes (modified by VSCode)

--- CubeVolume.java
+++ CubeVolume.java
@@ -5,14 +5,14 @@
 import java.util.Scanner;
 public class CubeVolume {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Enter the length of one side of the cube: ");
-        double sideLength = input.nextDouble();
-        
-        double volume = Math.pow(sideLength, 3);
-        
-        System.out.println("Volume = " + volume + " units³");
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the length of one side of the cube: ");
+            double sideLength = input.nextDouble();
+            
+            double volume = Math.pow(sideLength, 3);
+            
+            System.out.println("Volume = " + volume + " units³");
+        }
     }
 
     
